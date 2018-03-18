11249e9a8cc005cf60b5a5bb4504ac7242dd57c4
Huub <huubhoofs@gmail.com>
2018-03-18 21:47:08 +0100

    small

--- car_check.py
+++ car_check.py
@@ -14,18 +14,42 @@ model = VGG16(weights='imagenet')
 
 
 def preprocess(img):
+    """
+    preprocess image in order to make it VGG16 compatible
+
+    :param img: PIL image
+    :return: numpy array
+    """
+    # resize to VGG16 dimensions
     x = img.resize((224,224))
+    # make array
     x = image.img_to_array(x)
+    # add dimension
     x = np.expand_dims(x, axis=0)
+
     return preprocess_input(x)
 
 
 def predict_vgg_16(x):
+    """
+    predict probs for image
+
+    :param x: array of image
+    :return: predictions for all classes
+    """
+    # predict with the use of the VGG16-model
     preds = model.predict(x)
+
     return decode_predictions(preds, top=1000)[0]
 
 
 def check_if_car(preds):
+    """
+    check if any indication that there is a car on the image
+
+    :param preds: list of list with ref, label, and prop
+    :return: True if indication that car
+    """
     for _, label, prop in preds:
         if any(label == lab for lab in LABEL):
             if prop > .1:
@@ -33,9 +57,10 @@ def check_if_car(preds):
                 return True
 
 
-def if_car_save(img, preds):
-    if check_if_car(preds):
-        return True
+# def if_car_save(img, preds):
+#     """"""
+#     if check_if_car(preds):
+#         return True
 
 
 def main():
@@ -48,10 +73,12 @@ def main():
             with Image.open(os.path.join('screens', pic)) as img_resized:
                 img_preprocces = preprocess(img_resized)
                 img_predict = predict_vgg_16(img_preprocces)
-                if if_car_save(img_resized, img_predict):
-                    os.rename(os.path.join('screens', pic), os.path.join('/Users/HuCa/Dropbox/cars', pic))
+                if check_if_car(img_predict):
+                    print('hoi')
+                    # os.rename(os.path.join('screens', pic), os.path.join('/Users/HuCa/Dropbox/cars', pic))
                 else:
-                    os.remove(os.path.join('screens', pic))
+                    print('hoi')
+                    # os.remove(os.path.join('screens', pic))
 
 
 if __name__ == '__main__':
